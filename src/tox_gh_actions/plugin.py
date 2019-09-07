3e30c48f0a78483363dd493c74d99f3216e1b552
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2019-09-07 18:25:54 +0900

    Parse dictionary

--- src/tox_gh_actions/plugin.py
+++ src/tox_gh_actions/plugin.py
@@ -26,7 +26,7 @@ def tox_configure(config):
     ini = config._cfg
     section = ini.sections.get('gh-actions', {})
     verbosity0("section: {}".format(section))
-    python = section.get('python', {})
+    python = parse_dict(section.get('python', ''))
     verbosity0("python: {}".format(python))
     verbosity0("envlist: {}".format(config.envlist))
     verbosity0("envlist_default: {}".format(config.envlist_default))
@@ -35,3 +35,25 @@ def tox_configure(config):
 @hookimpl
 def tox_runtest_post(venv):
     verbosity0("cinderella is {}".format(venv.envconfig.cinderella))
+
+
+# From https://github.com/tox-dev/tox-travis/blob/master/src/tox_travis/utils.py
+# https://github.com/tox-dev/tox-travis/blob/master/LICENSE
+
+def parse_dict(value):
+    """Parse a dict value from the tox config.
+    .. code-block: ini
+        [travis]
+        python =
+            2.7: py27, docs
+            3.5: py{35,36}
+    With this config, the value of ``python`` would be parsed
+    by this function, and would return::
+        {
+            '2.7': 'py27, docs',
+            '3.5': 'py{35,36}',
+        }
+    """
+    lines = [line.strip() for line in value.strip().splitlines()]
+    pairs = [line.split(':', 1) for line in lines if line]
+    return dict((k.strip(), v.strip()) for k, v in pairs)
