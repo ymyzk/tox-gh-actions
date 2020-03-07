d2a3fc5f211a900d18efad74918d90c259dfedf3
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2020-03-07 22:09:07 +0900

    Add PyPy support

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -86,3 +86,28 @@ def test_get_factors(config, factors, expected):
 ])
 def test_get_envlist_from_factors(envlist, factors, expected):
     assert plugin.get_envlist_from_factors(envlist, factors) == expected
+
+
+@pytest.mark.parametrize("version,info,expected", [
+    (
+        "3.8.1 (default, Jan 22 2020, 06:38:00) \n[GCC 9.2.0]",
+        (3, 8, 1, "final", 0),
+        "3.8",
+    ),
+    (
+        "3.6.9 (1608da62bfc7, Dec 23 2019, 10:50:04)\n"
+        "[PyPy 7.3.0 with GCC 7.3.1 20180303 (Red Hat 7.3.1-5)]",
+        (3, 6, 9, "final", 0),
+        "pypy3",
+    ),
+    (
+        "2.7.13 (724f1a7d62e8, Dec 23 2019, 15:36:24)\n"
+        "[PyPy 7.3.0 with GCC 7.3.1 20180303 (Red Hat 7.3.1-5)]",
+        (2, 7, 13, "final", 42),
+        "pypy2",
+    )
+])
+def test_get_version(mocker, version, info, expected):
+    mocker.patch("tox_gh_actions.plugin.sys.version", version)
+    mocker.patch("tox_gh_actions.plugin.sys.version_info", info)
+    assert plugin.get_python_version() == expected
