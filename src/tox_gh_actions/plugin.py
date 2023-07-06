b56c38e0bb2cbeff5a7f796109270c4d9a56ffb1
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2023-07-06 23:20:51 +0900

    Fix warning message when tox or tox -p 0 is used to run tox

--- src/tox_gh_actions/plugin.py
+++ src/tox_gh_actions/plugin.py
@@ -211,6 +211,10 @@ def is_log_grouping_enabled(options: Parsed) -> bool:
         elif isinstance(options.parallel, int) and options.parallel > 0:
             # Case for `tox p` or `tox -p <num>`
             return False
+        elif isinstance(options.parallel, int) and options.parallel = 0:
+            # Case for `tox` or `tox -p 0`
+            # tox will disable the parallel execution
+            return True
         logger.warning(
             "tox-gh-actions couldn't understand the parallel option. "
             "ignoring the given option: %s",
